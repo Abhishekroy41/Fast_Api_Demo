d1b0b35121f0f674e8b00311e24c18587ea3c3c9
Abhishek Kumar Roy <112243881+Abhishekroy41@users.noreply.github.com>
2026-03-05 22:44:30 +0530

    Update database URL to use a password

--- database.py
+++ database.py
@@ -2,6 +2,6 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-db_url = "postgresql://postgres:1234@localhost:5432/cust"
+db_url = "postgresql://postgres:"password"@localhost:5432/cust"
 engine = create_engine(db_url)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
